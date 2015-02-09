7090d3b94c0392707cc48f8ed5c5fc3046b27552
class <class@CST_N417_16.cst.citytech.cuny.edu>
2015-02-09 17:07:05 -0500

    fixed the errors

--- Lab1/src/lab1/SalesTax.java
+++ Lab1/src/lab1/SalesTax.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 /**
  * This program calculates the total price which includes sales tax.
  *
- * @author <a href="mailto:rkhatchadourian@citytech.cuny.edu">Raffi
- * Khatchadourian</a>
+ * @author <a href="mailto:Anu.Alli@mail.citytech.cuny.edu">Raffi
+ * AnuAlli </a>
  */
 public class SalesTax {
 
